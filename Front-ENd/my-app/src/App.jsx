af8781a24a90a39cb8d4ce7afabfa79f3b11d233
vijayakumar1069 <vijay.r20799@gmail.com>
2024-01-10 12:59:11 +0530

    Navbar UI completed successfully

--- Front-ENd/my-app/src/App.jsx
+++ Front-ENd/my-app/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className='text-3xl text-red-700'>Hello</h1>
-    
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
